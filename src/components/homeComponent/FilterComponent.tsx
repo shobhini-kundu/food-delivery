5ef41ec4da72d9905860af77e68f53fd936d1879
shobhini-kundu <shobhinikundu@gmail.com>
2025-06-17 18:13:45 +0530

    new changes

---
+++ src/components/homeComponent/FilterComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./FilterComponent.scss"
+import { IoIosSearch  } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
+const FilterComponent:React.FC = () => {
+  return (
+    <>
+        <div className='filter_component'>
+            <div className="filter_section">
+                <div className="location_container">
+                    <div className="location_title">Your Location</div>
+                    <div className="location">Lorem, ipsum dolor.</div>
+                </div>
+                <div className="search_container">
+                    <div className="search_icon">< IoIosSearch  className='icon'/></div>
+                    <input type="text" className="search_input" placeholder='Search Restaurant'/>
+                    <div className="cross_icon"><RxCross2 className='icon'/></div>
+                </div>
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default FilterComponent
--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,10 +1,19 @@
 import { IonContent, IonPage } from '@ionic/react';
 import './HomeScreen.scss';
+import FilterComponent from '../components/homeComponent/FilterComponent';
 
 const HomeScreen: React.FC = () => {
   return (
     <IonPage>
     <IonContent fullscreen>
+      <div className="home_screen">
+        <div className="header_section">
+          <FilterComponent/>
+        </div>
+        <div className="body_section">
+          
+        </div>
+      </div>
     </IonContent>
     </IonPage>
   );
